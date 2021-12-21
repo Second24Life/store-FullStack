57f9ee86c5863c238a6dae0d6099428892a5842d
Second24Life <maka123456789@mail.ru>
2021-12-22 03:10:23 +0700

    add controllers, middleware, ApiError.

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,19 @@
+const { Device } = require('../models/models');
+const ApiError = require('../error/ApiError');
+const uuid =require('uuid');
+
+class DeviceController {
+    async create(req, res) {
+        const {name, price, brandId, typeId, info} = req.body;
+        const {img} = req.files;
+        let fileName = uuid.v4() + ".jpg";
+    }
+    async getAll(req, res) {
+
+    }
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports = new DeviceController();
